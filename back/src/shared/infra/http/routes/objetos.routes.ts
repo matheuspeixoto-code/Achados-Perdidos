56676339fa46c6f09f3fdb312e16f519284b7bdc
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-24 20:05:26 -0300

    adicionando a rota de requisicao de objetos por id

--- back/src/shared/infra/http/routes/objetos.routes.ts
+++ back/src/shared/infra/http/routes/objetos.routes.ts
@@ -1,14 +1,16 @@
 import { CreateObjetosController } from "@modules/objetos/useCases/createObjetos/CreateObjetosController";
 import { ListObjetosController } from "@modules/objetos/useCases/listObjetos/ListObjetosController";
+import { ListObjetosByIdController } from "@modules/objetos/useCases/listObjetoById/ListObjetoByIdController";
 import { Router } from "express";
 
 const objetosRouter = Router()
 
 const createObjetosController = new CreateObjetosController()
 const listObjetosController = new ListObjetosController()
+const listObjetosByIdController = new ListObjetosByIdController ()
 
 objetosRouter.post("/",createObjetosController.handle)
 objetosRouter.get("/",listObjetosController.handle)
-
+objetosRouter.get("/:id",listObjetosByIdController.handle)
 
 export {objetosRouter}
