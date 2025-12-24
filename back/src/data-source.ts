2001c1268ff26b2c31cb658a130f41aa83f15173
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-23 21:07:09 -0300

    modificando as variaveis de conexao com o DB

--- back/src/data-source.ts
+++ back/src/data-source.ts
@@ -1,9 +1,11 @@
 import { DataSource } from "typeorm";
+import "dotenv/config";
+
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
