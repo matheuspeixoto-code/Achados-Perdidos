cd1131e0ae70964fe45423c52f01a756dac05e1c
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-24 17:55:09 -0300

    inerindo o container de dependências de Categoria

--- back/src/shared/container/index.ts
+++ back/src/shared/container/index.ts
@@ -2,8 +2,15 @@ import { container } from "tsyringe";
 
 import { IObjetosRepository } from "@modules/objetos/implementations/IObjetosRepository";
 import { ObjetosRepository } from "@modules/objetos/infra/typeorm/repository/ObjetosRepository";
+import { ICategoriaRepository } from "@modules/objetos/implementations/ICategoriaRepository";
+import { CategoriaRepository } from "@modules/objetos/infra/typeorm/repository/CategoriaRepository";
 
 container.registerSingleton<IObjetosRepository>(
     "ObjetosRepository",
     ObjetosRepository
+)
+
+container.registerSingleton<ICategoriaRepository>(
+    "CategoriaRepository",
+    CategoriaRepository
 )
