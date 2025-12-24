a47005b1f8b064777e1ced415bd0eb99ec9c1cdc
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-24 19:03:37 -0300

    implementando a rota de get

--- back/src/shared/infra/http/routes/objetos.routes.ts
+++ back/src/shared/infra/http/routes/objetos.routes.ts
@@ -1,11 +1,14 @@
 import { CreateObjetosController } from "@modules/objetos/useCases/createObjetos/CreateObjetosController";
+import { ListObjetosController } from "@modules/objetos/useCases/listObjetos/ListObjetosController";
 import { Router } from "express";
 
 const objetosRouter = Router()
 
 const createObjetosController = new CreateObjetosController()
+const listObjetosController = new ListObjetosController()
 
 objetosRouter.post("/",createObjetosController.handle)
+objetosRouter.get("/",listObjetosController.handle)
 
 
 export {objetosRouter}
