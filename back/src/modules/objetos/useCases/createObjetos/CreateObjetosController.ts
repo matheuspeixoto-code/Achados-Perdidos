6ccbc8e8135b0d65d92d2ab298cbca0177d8c647
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-24 18:37:43 -0300

    alterando  o controller para receber e enviar ao useCase o categoria_id

--- back/src/modules/objetos/useCases/createObjetos/CreateObjetosController.ts
+++ back/src/modules/objetos/useCases/createObjetos/CreateObjetosController.ts
@@ -4,12 +4,12 @@ import { CreateObjetosUseCase } from "./CreateObjetosUseCase"
 
 class CreateObjetosController{
     async handle(request:Request,response:Response):Promise<Response>{
-        const {nome,descricao,local,dataEncontrada} = request.body
+        const {nome,descricao,local,dataEncontrada,categoria_id} = request.body
 
         const createObjetosUseCase = container.resolve(CreateObjetosUseCase)
 
         const objeto =await createObjetosUseCase.execute({
-            nome,descricao,local,dataEncontrada
+            nome,descricao,local,dataEncontrada,categoria_id
         })
 
         return response.status(201).json(objeto)
