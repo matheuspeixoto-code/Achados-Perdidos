d7ae2228ddff71111dc34a82bb3bf52a1b0e8711
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-24 18:13:47 -0300

    inserindo a rota de get categorias

--- back/src/shared/infra/http/routes/categorias.routes.ts
+++ back/src/shared/infra/http/routes/categorias.routes.ts
@@ -1,12 +1,14 @@
 import { CreateCategoriaController } from "@modules/objetos/useCases/createCategoria/CreateCategoriaController";
+import { ListCategoriaController } from "@modules/objetos/useCases/listCategoria/ListCategoriaController";
 import { Router } from "express";
 
 const categoriaRouter = Router()
 
 const createCategoriaController = new CreateCategoriaController()
+const listCategoriaController = new ListCategoriaController()
 
 categoriaRouter.post("/",createCategoriaController.handle)
-categoriaRouter.get("/")
+categoriaRouter.get("/",listCategoriaController.handle)
 
 
 export {categoriaRouter}
