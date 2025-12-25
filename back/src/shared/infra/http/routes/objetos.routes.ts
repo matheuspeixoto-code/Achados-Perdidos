409ec92ac1ea082a2cc2e76bd2ccab5ab09182c1
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-25 18:55:41 -0300

    implementando a rota de upload images

--- back/src/shared/infra/http/routes/objetos.routes.ts
+++ back/src/shared/infra/http/routes/objetos.routes.ts
@@ -1,16 +1,28 @@
 import { CreateObjetosController } from "@modules/objetos/useCases/createObjetos/CreateObjetosController";
 import { ListObjetosController } from "@modules/objetos/useCases/listObjetos/ListObjetosController";
 import { ListObjetosByIdController } from "@modules/objetos/useCases/listObjetoById/ListObjetoByIdController";
+import { UploadObjetoImageController } from "@modules/objetos/useCases/uploadObjetoImage/UploadObjetoImageController";
+import multer from "multer";
 import { Router } from "express";
 
+import uploadConfig from "@config/upload";
 const objetosRouter = Router()
+
+const upload= multer(uploadConfig.upload("./tmp/objetos"))
 
 const createObjetosController = new CreateObjetosController()
 const listObjetosController = new ListObjetosController()
 const listObjetosByIdController = new ListObjetosByIdController ()
+const uploadObjetoImageController = new UploadObjetoImageController()
 
 objetosRouter.post("/",createObjetosController.handle)
 objetosRouter.get("/",listObjetosController.handle)
 objetosRouter.get("/:id",listObjetosByIdController.handle)
+
+objetosRouter.post("/images/:id",
+    upload.array("images"),
+    uploadObjetoImageController.handle
+
+)
 
 export {objetosRouter}
