0efae4673133b20ec93bd1c0d17375e9484898c9
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-25 19:02:53 -0300

    implementando o container de dependencias de ObjetosImage

--- back/src/shared/container/index.ts
+++ back/src/shared/container/index.ts
@@ -4,10 +4,18 @@ import { IObjetosRepository } from "@modules/objetos/implementations/IObjetosRep
 import { ObjetosRepository } from "@modules/objetos/infra/typeorm/repository/ObjetosRepository";
 import { ICategoriaRepository } from "@modules/objetos/implementations/ICategoriaRepository";
 import { CategoriaRepository } from "@modules/objetos/infra/typeorm/repository/CategoriaRepository";
+import { IObjetosImageRepository } from "@modules/objetos/implementations/IObjetosImageRepository";
+import { ObjetosImagesRepository } from "@modules/objetos/infra/typeorm/repository/ObjetoImageRepository";
+
 
 container.registerSingleton<IObjetosRepository>(
     "ObjetosRepository",
     ObjetosRepository
+)
+
+container.registerSingleton<IObjetosImageRepository>(
+    "ObjetosImagesRepository",
+    ObjetosImagesRepository
 )
 
 container.registerSingleton<ICategoriaRepository>(
