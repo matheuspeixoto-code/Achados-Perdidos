ba8287d4b09734a8943b9af703fdb822a741f9c2
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-26 18:17:38 -0300

    implementando a rota de criação de usuário

--- back/src/shared/infra/http/routes/index.ts
+++ back/src/shared/infra/http/routes/index.ts
@@ -2,10 +2,13 @@ import { Router } from "express";
 
 import { objetosRouter } from "./objetos.routes";
 import { categoriaRouter } from "./categorias.routes";
+import { userRouter } from "./user.routes";
+
 
 const router = Router()
 
 router.use("/Objetos", objetosRouter)
 router.use("/Categoria", categoriaRouter)
+router.use("/User",userRouter)
 
 export {router}
