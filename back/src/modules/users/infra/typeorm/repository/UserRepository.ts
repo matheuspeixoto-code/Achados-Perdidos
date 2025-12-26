48c03d15284218cc5ac63a531d1f590a0edc4ed2
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-26 18:19:31 -0300

    implementando algumas metodos basicos do UserRepository

---
+++ back/src/modules/users/infra/typeorm/repository/UserRepository.ts
@@ -0,0 +1,39 @@
+import { ICreateUserDTO } from "@modules/users/dtos/ICreateUserDTO";
+import { IUserReposiory } from "@modules/users/implementations/IUserRepository";
+import { User } from "../entities/User";
+import { Repository } from "typeorm";
+import { AppDataSource } from "@data";
+
+
+class UserRepository implements IUserReposiory{
+    private repository : Repository<User>
+
+    constructor(){
+        this.repository = AppDataSource.getRepository(User)
+    }
+
+    async create({cpf,data_nascimento,email,senha,genero,telefone,nome_completo,username}: ICreateUserDTO): Promise<User> {
+        const user = this.repository.create({
+            cpf,
+            data_nascimento,
+            email,
+            senha,
+            genero,
+            telefone,
+            nome_completo,
+            username
+        });
+
+        await this.repository.save(user)
+
+        return user
+    }
+
+    async findByEmail(email: string): Promise<User>{
+        const user = await this.repository.findOne({where:{email}});
+        return user;  
+    }
+
+}
+
+export {UserRepository}
