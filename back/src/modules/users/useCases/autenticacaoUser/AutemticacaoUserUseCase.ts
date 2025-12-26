c803c61caeff8a3be48005ee695c5169c70ee22b
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-26 19:57:32 -0300

    implementando o controller e UseCase de autenticacao de usuario

---
+++ back/src/modules/users/useCases/autenticacaoUser/AutemticacaoUserUseCase.ts
@@ -0,0 +1,52 @@
+import { IUserReposiory } from "@modules/users/implementations/IUserRepository";
+import { User } from "@modules/users/infra/typeorm/entities/User";
+import { AppError } from "@shared/infra/errors/AppError";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+interface IRequest{
+    email:string;
+    senha:string;
+}
+
+interface IResponse{
+    user:User,
+    token:string;
+}
+
+@injectable()
+class AutenticacaoUserUseCase{
+    constructor(
+        @inject("UserRepository")
+        private userRepository:IUserReposiory
+    ){}
+
+    async execute({email,senha}:IRequest):Promise<IResponse>{
+        const user = await this.userRepository.findByEmail(email)
+
+        if(!user){
+            throw new AppError("Email ou senha incorreta")
+        }
+
+        const senhaMatch = await compare(senha,user.senha)
+
+        if(!senhaMatch){
+            throw new AppError("Email ou senha incorreta")
+        }
+
+        const token = sign({},process.env.JWT_SECRET,{
+            subject:user.id,
+            expiresIn:"1d"
+        })
+
+        const tokenReturn:IResponse = {
+            token,
+            user
+        }
+
+        return tokenReturn
+    }
+}
+
+export {AutenticacaoUserUseCase}
---
+++ back/src/modules/users/useCases/autenticacaoUser/AutenticacaoUserController.ts
@@ -0,0 +1,18 @@
+import { Request,Response } from "express"
+import { container } from "tsyringe";
+import { AutenticacaoUserUseCase } from "./AutemticacaoUserUseCase";
+
+
+class AutenticacaoUserController{
+    async handle(request:Request,response:Response):Promise<Response>{
+        const {email,senha} = request.body;
+
+        const autenticacaoUserUseCase = container.resolve(AutenticacaoUserUseCase)
+
+        const token = await autenticacaoUserUseCase.execute({email,senha})
+
+        return response.json(token)
+    }
+}
+
+export {AutenticacaoUserController}
