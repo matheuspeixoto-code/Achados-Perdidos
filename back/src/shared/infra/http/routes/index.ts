950963c85ffb4bff19e8d2764c850870adb889c1
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-26 19:55:53 -0300

    trabalhando com rota de secao de usuario

--- back/src/shared/infra/http/routes/index.ts
+++ back/src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { objetosRouter } from "./objetos.routes";
 import { categoriaRouter } from "./categorias.routes";
 import { userRouter } from "./user.routes";
+import { autenticacaoRouter } from "./autenticacao.routes";
 
 
 const router = Router()
@@ -10,5 +11,6 @@ const router = Router()
 router.use("/Objetos", objetosRouter)
 router.use("/Categoria", categoriaRouter)
 router.use("/User",userRouter)
+router.use(autenticacaoRouter)
 
 export {router}
