cfef09ec6ec761fb7c30c964233156d09ae0d41d
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-26 20:08:40 -0300

    inserindo o metodo de busca por id na interface

--- back/src/modules/users/implementations/IUserRepository.ts
+++ back/src/modules/users/implementations/IUserRepository.ts
@@ -5,6 +5,7 @@ import { User } from "../infra/typeorm/entities/User"
 interface IUserReposiory{
     create(data:ICreateUserDTO): Promise<User>;
     findByEmail(email:string):Promise<User>;
+    findById(id:string):Promise<User>;
 }
 
 export {IUserReposiory}
