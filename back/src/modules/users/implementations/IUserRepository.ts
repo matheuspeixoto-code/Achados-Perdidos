97645eb3b3637ef4122f063fb7fd41d472807e04
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-27 20:10:49 -0300

    inserindo um novo metodo à interface

--- back/src/modules/users/implementations/IUserRepository.ts
+++ back/src/modules/users/implementations/IUserRepository.ts
@@ -6,6 +6,7 @@ interface IUserReposiory{
     create(data:ICreateUserDTO): Promise<User>;
     findByEmail(email:string):Promise<User>;
     findById(id:string):Promise<User>;
+    save(user:User):Promise<User>;
 }
 
 export {IUserReposiory}
