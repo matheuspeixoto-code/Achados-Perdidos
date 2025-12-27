314b100ab4a1c70662b250fde64b10743b01cdee
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-27 20:17:55 -0300

    implementando o middleware para verificar se o usuario esta logado

---
+++ back/src/shared/infra/http/middlewares/verificarAutentificacao.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { UserRepository } from "@modules/users/infra/typeorm/repository/UserRepository";
+import { AppError } from "@shared/infra/errors/AppError";
+
+interface IPayload{
+    sub:string
+}
+
+export async function verificarAutentificacao(request:Request,response:Response,next:NextFunction){
+    const authHeader = request.headers.authorization;
+
+    if(!authHeader){
+        throw new AppError("Token missing",401)
+    }
+
+    const [,token]= authHeader.split(" ")
+
+    try{
+        const {sub :user_id} =verify(token,process.env.JWT_SECRET) as IPayload
+        const userRepository = new UserRepository()
+        const user= userRepository.findById(user_id)
+        
+        if(!user){
+            throw new AppError("Usuário não existente", 401)
+        }
+
+        request.user={
+            id:user_id
+        }
+
+        next()
+
+    }catch{
+        throw new AppError("Invalid token",401)
+    }
+}
