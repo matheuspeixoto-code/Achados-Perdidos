a0bef1887b79afc33fcc4810b391d29e1c95847c
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-27 20:13:56 -0300

    implementado a rota de inserir avatar

--- back/src/shared/infra/http/routes/user.routes.ts
+++ back/src/shared/infra/http/routes/user.routes.ts
@@ -1,12 +1,27 @@
 import { CreateUserController } from "@modules/users/useCases/createUser/CreateUsersController";
+import { verificarAutentificacao } from "../middlewares/verificarAutentificacao";
+
+import uploadConfig from "@config/upload";
 import { Router } from "express";
+import multer from "multer";
+import { UploadUserAvatarController } from "@modules/users/useCases/uploadUserAvatar/UploadUserAvatarController";
 
 const userRouter = Router()
 
+
 const createUserController = new CreateUserController()
+const uploadUserAvatarController = new UploadUserAvatarController()
+
+const uploadAvatar=multer(uploadConfig.upload("./tmp/avatar"))
 
 
 userRouter.post("/createUser",createUserController.handle)
+
+userRouter.patch("/avatar",
+    verificarAutentificacao,
+    uploadAvatar.single("avatar"),
+    uploadUserAvatarController.handle
+)
 
 
 export {userRouter}
