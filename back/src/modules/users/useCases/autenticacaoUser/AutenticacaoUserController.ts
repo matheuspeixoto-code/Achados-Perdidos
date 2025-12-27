54178c665134e79abc2bc7d2157028b678c71c2e
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-27 20:12:57 -0300

    refatorando o import

--- back/src/modules/users/useCases/autenticacaoUser/AutenticacaoUserController.ts
+++ back/src/modules/users/useCases/autenticacaoUser/AutenticacaoUserController.ts
@@ -1,6 +1,6 @@
 import { Request,Response } from "express"
 import { container } from "tsyringe";
-import { AutenticacaoUserUseCase } from "./AutemticacaoUserUseCase";
+import { AutenticacaoUserUseCase } from "./AutenticacaoUserUseCase";
 
 
 class AutenticacaoUserController{
