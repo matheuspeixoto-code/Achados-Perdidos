681ba58aa91927156664d7470e613c23b3d4e464
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-29 17:11:25 -0300

    verificando se o usuario é adm nas rotas nescessarias

--- back/src/shared/infra/http/routes/categorias.routes.ts
+++ back/src/shared/infra/http/routes/categorias.routes.ts
@@ -1,13 +1,19 @@
 import { CreateCategoriaController } from "@modules/objetos/useCases/createCategoria/CreateCategoriaController";
 import { ListCategoriaController } from "@modules/objetos/useCases/listCategoria/ListCategoriaController";
 import { Router } from "express";
+import { verificarAutentificacao } from "../middlewares/verificarAutentificacao";
+import { verificarAdmin } from "../middlewares/verificarAdmin";
 
 const categoriaRouter = Router()
 
 const createCategoriaController = new CreateCategoriaController()
 const listCategoriaController = new ListCategoriaController()
 
-categoriaRouter.post("/",createCategoriaController.handle)
+categoriaRouter.post("/",
+    verificarAutentificacao,
+    verificarAdmin,
+    createCategoriaController.handle
+)
 categoriaRouter.get("/",listCategoriaController.handle)
 
 
--- back/src/shared/infra/http/routes/objetos.routes.ts
+++ back/src/shared/infra/http/routes/objetos.routes.ts
@@ -6,6 +6,8 @@ import multer from "multer";
 import { Router } from "express";
 
 import uploadConfig from "@config/upload";
+import { verificarAutentificacao } from "../middlewares/verificarAutentificacao";
+import { verificarAdmin } from "../middlewares/verificarAdmin";
 const objetosRouter = Router()
 
 const upload= multer(uploadConfig.upload("./tmp/objetos"))
@@ -15,11 +17,17 @@ const listObjetosController = new ListObjetosController()
 const listObjetosByIdController = new ListObjetosByIdController ()
 const uploadObjetoImageController = new UploadObjetoImageController()
 
-objetosRouter.post("/",createObjetosController.handle)
+objetosRouter.post("/",
+    verificarAutentificacao,
+    verificarAdmin,
+    createObjetosController.handle
+)
 objetosRouter.get("/",listObjetosController.handle)
 objetosRouter.get("/:id",listObjetosByIdController.handle)
 
 objetosRouter.post("/images/:id",
+    verificarAutentificacao,
+    verificarAdmin,
     upload.array("images"),
     uploadObjetoImageController.handle
 
