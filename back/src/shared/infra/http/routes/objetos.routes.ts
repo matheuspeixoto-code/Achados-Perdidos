a0d4cc6055d1141d7ff22588a3ba6209f430899b
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-29 19:43:10 -0300

    inserindo as rotas

--- back/src/shared/infra/http/routes/objetos.routes.ts
+++ back/src/shared/infra/http/routes/objetos.routes.ts
@@ -8,6 +8,7 @@ import { Router } from "express";
 import uploadConfig from "@config/upload";
 import { verificarAutentificacao } from "../middlewares/verificarAutentificacao";
 import { verificarAdmin } from "../middlewares/verificarAdmin";
+import { UpdateObjetosController } from "@modules/objetos/useCases/updateObjeto/UpdateObjetoController";
 const objetosRouter = Router()
 
 const upload= multer(uploadConfig.upload("./tmp/objetos"))
@@ -16,12 +17,20 @@ const createObjetosController = new CreateObjetosController()
 const listObjetosController = new ListObjetosController()
 const listObjetosByIdController = new ListObjetosByIdController ()
 const uploadObjetoImageController = new UploadObjetoImageController()
+const updateObjetoController = new UpdateObjetosController()
 
 objetosRouter.post("/",
     verificarAutentificacao,
     verificarAdmin,
     createObjetosController.handle
 )
+
+objetosRouter.put("/:id",
+    verificarAutentificacao,
+    verificarAdmin,
+    updateObjetoController.handle
+)
+
 objetosRouter.get("/",listObjetosController.handle)
 objetosRouter.get("/:id",listObjetosByIdController.handle)
 
--- back/src/shared/infra/http/routes/user.routes.ts
+++ back/src/shared/infra/http/routes/user.routes.ts
@@ -5,17 +5,24 @@ import uploadConfig from "@config/upload";
 import { Router } from "express";
 import multer from "multer";
 import { UploadUserAvatarController } from "@modules/users/useCases/uploadUserAvatar/UploadUserAvatarController";
+import { GetUserByIdController } from "@modules/users/useCases/getUserById/GetUserByIdController";
 
 const userRouter = Router()
 
 
 const createUserController = new CreateUserController()
 const uploadUserAvatarController = new UploadUserAvatarController()
+const getUserByIdController = new GetUserByIdController()
 
 const uploadAvatar=multer(uploadConfig.upload("./tmp/avatar"))
 
 
 userRouter.post("/createUser",createUserController.handle)
+userRouter.get("/:id",
+    verificarAutentificacao,
+    getUserByIdController.handle
+
+)
 
 userRouter.patch("/avatar",
     verificarAutentificacao,
