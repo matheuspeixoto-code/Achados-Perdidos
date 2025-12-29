b6b015522d9d3a669f0404976d157c345de0538e
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-29 17:10:37 -0300

    implementando o middlewares para verificar se o usuario é admin

---
+++ back/src/shared/infra/http/middlewares/verificarAdmin.ts
@@ -0,0 +1,23 @@
+import { AppError } from "@shared/infra/errors/AppError";
+import { UserRepository } from "@modules/users/infra/typeorm/repository/UserRepository";
+import { Request,Response,NextFunction } from "express"
+
+export async function verificarAdmin(
+    request:Request,
+    response:Response,
+    next:NextFunction
+
+){
+    const {id}= request.user;
+
+    const userRepository= new UserRepository()
+
+    const user = await userRepository.findById(id)
+
+    if(!user.isAdmin){
+        throw new AppError("User isn't admin!")
+    }
+
+    return next()
+}
+    
