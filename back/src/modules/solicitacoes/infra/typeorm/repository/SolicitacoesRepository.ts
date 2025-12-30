45a9b912c6fdec665ab9c738db3398cd4d779df4
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-30 20:52:53 -0300

    implementando os metodos de busca geral de solicitados,e por id

--- back/src/modules/solicitacoes/infra/typeorm/repository/SolicitacoesRepository.ts
+++ back/src/modules/solicitacoes/infra/typeorm/repository/SolicitacoesRepository.ts
@@ -6,12 +6,83 @@ import { ICreateSolicitacoesDTO } from "@modules/solicitacoes/dtos/ICreateSolici
 
 
 
+
 class SolicitacoesRepository implements ISolicitacoesRepository{
     private repository: Repository<SolicitacoesResgate>
 
     constructor(){
         this.repository= AppDataSource.getRepository(SolicitacoesResgate)
     }
+    async listSolicitadosById(id: string): Promise<SolicitacoesResgate> {
+        const solicitacao = await this.repository
+            .createQueryBuilder("s")
+            .leftJoinAndSelect("s.usuario", "u")
+            .leftJoinAndSelect("s.objeto", "o")
+            .leftJoinAndSelect("o.imagens", "imagens")
+            .leftJoinAndSelect("o.categoria_id", "categoria")
+            .select([
+
+            "s.id",
+            "s.descricao",
+            "s.imagem",
+            "s.status",
+            "s.created_at",
+            
+
+      
+            "u.id",
+            "u.nome_completo",
+            "u.avatar",
+            "u.email",
+
+            
+            "o.id",
+            "o.nome",
+            "o.descricao",
+            "o.local",
+            "o.hora",
+            "o.status",
+            "o.dataEncontrada",
+
+           
+            "categoria.id",
+            "categoria.nome",
+
+           
+            "imagens.id",
+            "imagens.objeto_image",
+            ])
+            .where("s.id = :id", { id })
+            .getOne();
+
+        return solicitacao;
+    }
+
+
+    async listSolicitados(): Promise<SolicitacoesResgate[]> {
+        const solicitacoes = await this.repository
+            .createQueryBuilder("s")
+            .leftJoin("s.usuario", "u")
+            .leftJoin("s.objeto", "o")
+            .select([
+                "s.id",
+                "s.status",
+                "s.created_at",
+
+                "u.id",
+                "u.nome_completo",
+                "u.avatar",
+
+                "o.id",
+                "o.nome",
+                "o.status",
+            ])
+            .getMany();
+
+        return solicitacoes;
+    }
+
+
     async findByObjetoId(objeto_id: string): Promise<SolicitacoesResgate> {
         return await this.repository.findOne({
             where: {
