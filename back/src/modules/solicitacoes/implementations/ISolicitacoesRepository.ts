ac02799e18ec2156351be58fb92c0f846d890f1c
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-30 20:51:50 -0300

    inseriondo os metodos de lista de solicitados no geral,e por id

--- back/src/modules/solicitacoes/implementations/ISolicitacoesRepository.ts
+++ back/src/modules/solicitacoes/implementations/ISolicitacoesRepository.ts
@@ -4,7 +4,9 @@ import { ICreateSolicitacoesDTO } from "../dtos/ICreateSolicitacoesDTO"
 
 interface ISolicitacoesRepository{
     create(data:ICreateSolicitacoesDTO):Promise<SolicitacoesResgate>;
-    findByObjetoId(id:string):Promise<SolicitacoesResgate>
+    findByObjetoId(id:string):Promise<SolicitacoesResgate>;
+    listSolicitados():Promise<SolicitacoesResgate[]>
+    listSolicitadosById(id:string):Promise<SolicitacoesResgate>
 }
 
 export {ISolicitacoesRepository}
