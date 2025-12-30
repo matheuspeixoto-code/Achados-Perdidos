dfd1bca21b15f0b0541c87de15c2d4fe31fb26c0
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-30 20:54:42 -0300

    inserindo as rotas de getPendentes para as solicitacoes em aberto

--- back/src/shared/infra/http/routes/solicitacao.routes.ts
+++ back/src/shared/infra/http/routes/solicitacao.routes.ts
@@ -3,11 +3,16 @@ import multer from "multer";
 import uploadConfig from "@config/upload";
 import { SolicitarObjetosController } from "@modules/solicitacoes/useCases/solicitarObjeto/SolicitarObjetoController";
 import { verificarAutentificacao } from "../middlewares/verificarAutentificacao";
+import { verificarAdmin } from "../middlewares/verificarAdmin";
+import { ListObjetosSolicitadosController } from "@modules/solicitacoes/useCases/listObjetosSolicitados/ListObjetosSolicitadosController";
+import { ListObjetosSolicitadosByIdController } from "@modules/solicitacoes/useCases/listObjetosSolicitadosById/ListObjetosSolicitadosByIdController";
 
 const solicitacoesRoutes = Router();
 const upload = multer(uploadConfig.upload("./tmp/solicitacao_image"));
 
 const solicitarObjetosController = new SolicitarObjetosController();
+const listObjetosSolicitadosController = new ListObjetosSolicitadosController()
+const listObjetosSolicitadosByIdController = new ListObjetosSolicitadosByIdController()
 
 solicitacoesRoutes.post(
   "/:objeto_id",
@@ -15,5 +20,17 @@ solicitacoesRoutes.post(
   upload.single("imagem"),
   solicitarObjetosController.handle
 );
+
+solicitacoesRoutes.get("/pendentes",
+  verificarAutentificacao,
+  verificarAdmin,
+  listObjetosSolicitadosController.handle
+)
+
+solicitacoesRoutes.get("/pendentes/:id",
+  verificarAutentificacao,
+  verificarAdmin,
+  listObjetosSolicitadosByIdController.handle
+)
 
 export { solicitacoesRoutes };
