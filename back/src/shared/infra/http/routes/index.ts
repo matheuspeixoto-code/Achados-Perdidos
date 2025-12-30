1f1f49fd61d23d5b86bd4a6d331187ce7b8a9999
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2025-12-30 18:47:27 -0300

    inserindo a rota de solicitacoes

--- back/src/shared/infra/http/routes/index.ts
+++ back/src/shared/infra/http/routes/index.ts
@@ -4,6 +4,7 @@ import { objetosRouter } from "./objetos.routes";
 import { categoriaRouter } from "./categorias.routes";
 import { userRouter } from "./user.routes";
 import { autenticacaoRouter } from "./autenticacao.routes";
+import { solicitacoesRoutes } from "./solicitacao.routes";
 
 
 const router = Router()
@@ -12,5 +13,6 @@ router.use("/Objetos", objetosRouter)
 router.use("/Categoria", categoriaRouter)
 router.use("/User",userRouter)
 router.use(autenticacaoRouter)
+router.use("/Solicitacoes",solicitacoesRoutes)
 
 export {router}
