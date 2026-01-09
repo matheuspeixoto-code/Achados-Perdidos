adfb0db726369b49bef70521ed1c452d960acd74
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-01-09 18:50:21 -0300

    inserindoa rota de delete

--- back/src/shared/infra/http/routes/user.routes.ts
+++ back/src/shared/infra/http/routes/user.routes.ts
@@ -6,6 +6,7 @@ import { Router } from "express";
 import multer from "multer";
 import { UploadUserAvatarController } from "@modules/users/useCases/uploadUserAvatar/UploadUserAvatarController";
 import { GetUserByIdController } from "@modules/users/useCases/getUserById/GetUserByIdController";
+import { DeleteUserController } from "@modules/users/useCases/deleteUser/DeleteUserController";
 
 const userRouter = Router()
 
@@ -13,6 +14,7 @@ const userRouter = Router()
 const createUserController = new CreateUserController()
 const uploadUserAvatarController = new UploadUserAvatarController()
 const getUserByIdController = new GetUserByIdController()
+const deleteUserController = new DeleteUserController()
 
 const uploadAvatar=multer(uploadConfig.upload("./tmp/avatar"))
 
@@ -21,6 +23,12 @@ userRouter.post("/createUser",createUserController.handle)
 userRouter.get("/:id",
     verificarAutentificacao,
     getUserByIdController.handle
+
+)
+
+userRouter.delete("/delete",
+    verificarAutentificacao,
+    deleteUserController.handle
 
 )
 
