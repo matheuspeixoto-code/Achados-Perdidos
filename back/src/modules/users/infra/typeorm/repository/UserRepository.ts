03c846ee066f63f6fd4d674cfb6b966fbb34088b
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-01-09 18:48:38 -0300

    implementando no repositorio de usuario o metodo de delete

--- back/src/modules/users/infra/typeorm/repository/UserRepository.ts
+++ back/src/modules/users/infra/typeorm/repository/UserRepository.ts
@@ -11,11 +11,50 @@ class UserRepository implements IUserReposiory{
     constructor(){
         this.repository = AppDataSource.getRepository(User)
     }
+    async deleteUser(user:User): Promise<void> {
+        await this.repository.softDelete(user.id);
+    }
     async save(user: User): Promise<User> {
         return await this.repository.save(user)
     }
 
-    async create({cpf,data_nascimento,email,senha,genero,telefone,nome_completo,username}: ICreateUserDTO): Promise<User> {
+    async create({
+        cpf,
+        data_nascimento,
+        email,
+        senha,
+        genero,
+        telefone,
+        nome_completo,
+        username,
+    }: ICreateUserDTO): Promise<User> {
+
+
+        const existingUser = await this.repository.findOne({
+            where: { email },
+            withDeleted: true,
+        });
+
+
+        if (existingUser && !existingUser.deletedAt) {
+            throw new Error("Email já cadastrado");
+        }
+
+
+        if (existingUser && existingUser.deletedAt) {
+            existingUser.deletedAt = null;
+            existingUser.cpf = cpf;
+            existingUser.data_nascimento = data_nascimento;
+            existingUser.senha = senha; 
+            existingUser.genero = genero;
+            existingUser.telefone = telefone;
+            existingUser.nome_completo = nome_completo;
+            existingUser.username = username;
+
+            return await this.repository.save(existingUser);
+        }
+
+
         const user = this.repository.create({
             cpf,
             data_nascimento,
@@ -24,13 +63,14 @@ class UserRepository implements IUserReposiory{
             genero,
             telefone,
             nome_completo,
-            username
+            username,
         });
 
-        await this.repository.save(user)
+        await this.repository.save(user);
 
-        return user
+        return user;
     }
+
 
     async findByEmail(email: string): Promise<User>{
         const user = await this.repository.findOne({where:{email}});
