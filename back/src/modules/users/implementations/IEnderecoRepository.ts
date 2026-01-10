e6bb5f84f5d3db7b4dde360fbb9d27508ac80bb5
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-01-09 21:47:07 -0300

    inserindo os metodos de busca e save de endereco

--- back/src/modules/users/implementations/IEnderecoRepository.ts
+++ back/src/modules/users/implementations/IEnderecoRepository.ts
@@ -1,7 +1,11 @@
 import { ICreateEnderecoDTO } from "../dtos/ICreateEnderecoDTO"
+import { IUpdateEndereco } from "../dtos/IUpdateEndereco"
+import { Endereco } from "../infra/typeorm/entities/Endereco"
 
 interface IEnderecoRepository{
     create(data:ICreateEnderecoDTO):Promise<void>
+    findByUserId(user_id:string):Promise<Endereco>
+    save(endereco:IUpdateEndereco):Promise<void>
 }
 
 export {IEnderecoRepository}
